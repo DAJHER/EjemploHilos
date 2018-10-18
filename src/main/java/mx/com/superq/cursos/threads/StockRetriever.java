f1f0f3c9057e878eb7beec6eeac3f932ecfbb301
Dann Joel <joelin_001@hotmail.com>
2018-10-18 13:05:56 -0500

    Depuracion de la clase StockRetriever

--- src/main/java/mx/com/superq/cursos/threads/StockRetriever.java
+++ src/main/java/mx/com/superq/cursos/threads/StockRetriever.java
@@ -5,26 +5,20 @@ import java.io.IOException;
 import java.math.BigDecimal;
 public class StockRetriever implements Runnable{
     private String compani;
+    StockRetriever(String compani){
+        this.compani = compani;
+    }
     @Override
     public void run() {
         try {
-            //TestStock.suma = TestStock.suma.add( getStockPrice());
-            getStockPrice();
-            //System.out.println(Thread.currentThread().getName() +);
+            Stock stock = YahooFinance.get(compani);
+            BigDecimal price = stock.getQuote().getPrice();
+            stock.print();
+            TestStock.suma = TestStock.suma.add(price);
             System.out.println(TestStock.suma);
         } catch (IOException ex) {
             System.out.println(ex.toString());
         }
     }
-    StockRetriever(String compani){this.compani = compani;}
-    public BigDecimal getStockPrice() throws IOException{
-        Stock stock = YahooFinance.get(compani);
-        BigDecimal price = stock.getQuote().getPrice();
-        stock.print();
-        TestStock.suma = TestStock.suma.add(price);
-        return price;
-    }
-    public String getCompani() {return compani;}
-    public void setCompani(String compani) {this.compani = compani;}
 }
 
